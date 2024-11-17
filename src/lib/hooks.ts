1ca1623b0299fde9da5819873de396dd7bc23454
shivam <shivamm0383@gmail.com>
2024-11-17 18:53:06 +0530

    custom-hook-for-single-jobItem

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { JobItem } from "./types";
+import { BASE_API_URL } from "./constants";
 
 export function useActiveId() {
   const [activeId, setActiveId] = useState<number | null>(null);
@@ -21,6 +22,22 @@ export function useActiveId() {
   return activeId;
 }
 
+export function useJobItem(id: number | null) {
+  const [jobItem, setJobItem] = useState(null);
+
+  useEffect(() => {
+    if (!id) return;
+    const fetchData = async () => {
+      const response = await fetch(`${BASE_API_URL}/${id}`);
+
+      const data = await response.json();
+      setJobItem(data.jobItem);
+      // console.log(data);
+    };
+    fetchData();
+  }, [id]);
+  return jobItem;
+}
 export function useJobItems(searchText: string) {
   const [jobItems, setJobItems] = useState<JobItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -30,9 +47,7 @@ export function useJobItems(searchText: string) {
     if (!searchText) return;
     const fetchData = async () => {
       setIsLoading(true);
-      const response = await fetch(
-        `https://bytegrad.com/course-assets/projects/rmtdev/api/data?search=${searchText}`
-      );
+      const response = await fetch(`${BASE_API_URL}?search=${searchText}`);
       const data = await response.json();
       setIsLoading(false);
       setJobItems(data.jobItems);
